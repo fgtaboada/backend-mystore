edbbf66970692de796394d7e93a8fc8579555912
fgtaboada <fgtaboada@gmail.com>
2024-06-19 01:00:37 +0200

    POST /orders/

--- routers/routerOrders.js
+++ routers/routerOrders.js
@@ -42,4 +42,35 @@ routerOrders.post( '/', async (req, res) => {
     res.json({ inserted: insertedOrder })
 })
 
+routerOrders.post( '/:idOrder/', async (req,res) => {
+    let idOrder = req.params.idOrder
+    
+    if ( idOrder == undefined ) {
+        return res.status(400).json({ error: 'no idOrder'})
+    }
+    
+    let idItem = req.body.idItem
+    if (idItem == undefined) {
+        return res.status(400).json({ error: 'no idItem in body' })
+    }
+
+    let units = req.body.units
+    if ( units == undefined ) {
+        return res.status(400).json({ error: 'no units in body' })
+    }
+    units = parseInt(units)
+
+    database.connect();
+    let itemsInOrder = await database.query( 'SELECT * FROM orders_items WHERE idOrder = ? AND idItem = ?',
+        [idOrder, idItem])
+    if( itemsInOrder.length > 0 ) {
+        // idItem already ordered -> increase one unit
+        return res.status(400).json({ error : "item already in the shopping bag"})
+    }
+    let orderItem = await database.query( 'INSERT INTO orders_items (idOrder,idItem,units) VALUES (?,?,?)', 
+        [idOrder,idItem,units] )
+    database.disconnect();
+    res.json({ inserted : true })
+})
+
 module.exports = routerOrders
